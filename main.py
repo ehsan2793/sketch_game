efe7c16c6c778f0b304aeb00e60e998a3aec0b5d
ehsan2793 <ehsan2793@gmail.com>
2022-02-21 23:43:28 -0800

    first push

---
+++ main.py
@@ -0,0 +1,10 @@
+from turtle import Turtle,Screen
+screen = Screen()
+jim = Turtle()
+# screen color and color settings
+screen.bgcolor('black')
+screen.colormode(255)
+
+
+jim.color((255,200,23))
+jim.forward(100)
